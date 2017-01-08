88f2ba896cc029d1934a4979ca7b3448407faae5
hanepjiv <hanepjiv@gmail.com>
2017-01-09 03:03:58 +0900

    lib.rs

--- src/error.rs
+++ src/error.rs
@@ -6,12 +6,8 @@
 //  @author hanepjiv <hanepjiv@gmail.com>
 //  @copyright The MIT License (MIT) / Apache License Version 2.0
 //  @since 2016/10/13
-//  @date 2016/11/11
+//  @date 2017/01/09
 
-// ////////////////////////////////////////////////////////////////////////////
-// extern  ====================================================================
-extern                  crate toml;
-// use  =======================================================================
 // ////////////////////////////////////////////////////////////////////////////
 // ============================================================================
 /// enum Error
@@ -36,11 +32,20 @@ impl ::std::fmt::Display for Error {
 }
 // ============================================================================
 impl ::std::error::Error for Error {
-    fn description(&self) -> &str { match self {
-        &Error::Column79Error(ref m)            => m.as_str(),
-        &Error::IOError(ref m, _)               => m.as_str(),
-        &Error::ParseConfigError(ref m, _)      => m.as_str(),
-        &Error::InvalidConfigError(ref m)       => m.as_str(),
-        &Error::InspectError(ref m)             => m.as_str(),
+    fn description(&self) -> &str { match *self {
+        Error::Column79Error(ref m)             => m.as_str(),
+        Error::IOError(ref m, _)                => m.as_str(),
+        Error::ParseConfigError(ref m, _)       => m.as_str(),
+        Error::InvalidConfigError(ref m)        => m.as_str(),
+        Error::InspectError(ref m)              => m.as_str(),
     } }
+    // ========================================================================
+    fn cause(&self) -> Option<&::std::error::Error> { match *self {
+        Error::Column79Error(_)                 => None,
+        Error::IOError(_, ref e)                => Some(e),
+        Error::ParseConfigError(_, _)           => None,
+        Error::InvalidConfigError(_)            => None,
+        Error::InspectError(_)                  => None,
+    } }
+
 }
