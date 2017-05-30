ff24c0fbc6571cc4919c76966a93aa10aa3b3715
hanepjiv <hanepjiv@gmail.com>
2017-05-30 21:04:37 +0900

    Update dependencies bitflags.

--- src/flags.rs
+++ src/flags.rs
@@ -6,12 +6,11 @@
 //  @author hanepjiv <hanepjiv@gmail.com>
 //  @copyright The MIT License (MIT) / Apache License Version 2.0
 //  @since 2016/10/15
-//  @date 2016/10/15
+//  @date 2017/05/30
 
 // ////////////////////////////////////////////////////////////////////////////
 // ============================================================================
 /// Flags
-bitflags! { #[allow(missing_docs)] pub flags Flags: u32 {
-    #[allow(missing_docs)]
-    const NOASK             = 0b00000000_00000000_00000000_00000001u32,
+bitflags! { #[allow(missing_docs)] pub struct Flags: u32 {
+    #[allow(missing_docs)] const NOASK                  = 0b00000001u32 << 0;
 } }
