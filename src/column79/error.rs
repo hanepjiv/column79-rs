e492ba9560c518ea0b501d6803ff3afbcda2cfa7
hanepjiv <hanepjiv@gmail.com>
2018-05-11 08:27:46 +0900

    "main() {" => "main() -> Result<()>"

---
+++ src/column79/error.rs
@@ -0,0 +1,84 @@
+// -*- mode:rust; coding:utf-8-unix; -*-
+
+//! error.rs
+
+//  Copyright 2017 hanepjiv
+//  @author hanepjiv <hanepjiv@gmail.com>
+//  @copyright The MIT License (MIT) / Apache License Version 2.0
+//  @since 2018/05/11
+//  @date 2018/05/11
+
+// ////////////////////////////////////////////////////////////////////////////
+// ============================================================================
+/// enum Error
+#[derive(Debug)]
+pub(crate) enum Error {
+    /// OptionNone
+    OptionNone(String),
+    /// GetOpts
+    GetOpts(::getopts::Fail),
+    /// StdIO
+    StdIO(::std::io::Error),
+    /// StdNumParseInt
+    StdNumParseInt(::std::num::ParseIntError),
+    /// Column79
+    Column79(::column79::Error),
+}
+// ============================================================================
+impl From<::getopts::Fail> for Error {
+    fn from(e: ::getopts::Fail) -> Self {
+        Error::GetOpts(e)
+    }
+}
+// ----------------------------------------------------------------------------
+impl From<::std::io::Error> for Error {
+    fn from(e: ::std::io::Error) -> Self {
+        Error::StdIO(e)
+    }
+}
+// ----------------------------------------------------------------------------
+impl From<::std::num::ParseIntError> for Error {
+    fn from(e: ::std::num::ParseIntError) -> Self {
+        Error::StdNumParseInt(e)
+    }
+}
+// ----------------------------------------------------------------------------
+impl From<::column79::Error> for Error {
+    fn from(e: ::column79::Error) -> Self {
+        Error::Column79(e)
+    }
+}
+// ============================================================================
+impl ::std::fmt::Display for Error {
+    // ========================================================================
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+// ============================================================================
+impl ::std::error::Error for Error {
+    // ========================================================================
+    fn description(&self) -> &str {
+        match *self {
+            Error::OptionNone(_) => "::column79::Error::OptionNone",
+            Error::GetOpts(ref e) => e.description(),
+            Error::StdIO(ref e) => e.description(),
+            Error::StdNumParseInt(ref e) => e.description(),
+            Error::Column79(ref e) => e.description(),
+        }
+    }
+    // ========================================================================
+    fn cause(&self) -> Option<&::std::error::Error> {
+        match *self {
+            Error::OptionNone(_) => None,
+            Error::GetOpts(ref e) => Some(e),
+            Error::StdIO(ref e) => Some(e),
+            Error::StdNumParseInt(ref e) => Some(e),
+            Error::Column79(ref e) => Some(e),
+        }
+    }
+}
+// ////////////////////////////////////////////////////////////////////////////
+// ============================================================================
+/// type Result
+pub(crate) type Result<T> = ::std::result::Result<T, Error>;
--- src/column79/main.rs
+++ src/column79/main.rs
@@ -6,7 +6,7 @@
 //  @author hanepjiv <hanepjiv@gmail.com>
 //  @copyright The MIT License (MIT) / Apache License Version 2.0
 //  @since 2016/10/12
-//  @date 2018/05/04
+//  @date 2018/05/11
 
 //! # Examples
 //!
@@ -15,26 +15,35 @@
 
 // ////////////////////////////////////////////////////////////////////////////
 // attribute  =================================================================
-#![deny(anonymous_parameters, box_pointers, missing_copy_implementations,
-        missing_debug_implementations, missing_docs, trivial_casts,
-        trivial_numeric_casts, unsafe_code, unstable_features,
-        unused_extern_crates, unused_import_braces, unused_qualifications,
-        unused_results, variant_size_differences, const_err, dead_code,
-        deprecated, illegal_floating_point_literal_pattern, improper_ctypes,
-        late_bound_lifetime_arguments, non_camel_case_types,
-        non_shorthand_field_patterns, non_snake_case, non_upper_case_globals,
-        no_mangle_generic_items, overflowing_literals, path_statements,
-        patterns_in_fns_without_body, plugin_as_library, private_in_public,
-        private_no_mangle_fns, private_no_mangle_statics,
-        renamed_and_removed_lints, stable_features, unconditional_recursion,
+#![deny(anonymous_parameters, bare_trait_object,
+        missing_copy_implementations, missing_debug_implementations,
+        missing_docs, unstable_features, unused_extern_crates,
+        unused_import_braces, unused_qualifications, unused_results,
+        variant_size_differences, const_err, deprecated,
+        illegal_floating_point_literal_pattern, improper_ctypes,
+        incoherent_fundamental_impls, late_bound_lifetime_arguments,
+        non_camel_case_types, non_shorthand_field_patterns, non_snake_case,
+        non_upper_case_globals, no_mangle_generic_items,
+        overflowing_literals, path_statements, patterns_in_fns_without_body,
+        plugin_as_library, private_in_public, private_no_mangle_fns,
+        private_no_mangle_statics, renamed_and_removed_lints,
+        safe_packed_borrows, stable_features, type_alias_bounds,
+        tyvar_behind_raw_pointer, unconditional_recursion,
         unions_with_drop_fields, unknown_lints, unreachable_code,
-        unreachable_patterns, unused_allocation, unused_assignments,
-        unused_attributes, unused_comparisons, unused_doc_comment,
-        unused_features, unused_imports, unused_macros, unused_must_use,
-        unused_mut, unused_parens, unused_unsafe, unused_variables,
-        while_true)]
-#![warn(dead_code)]
-#![allow(box_pointers, unsafe_code, trivial_casts, trivial_numeric_casts)]
+        unreachable_patterns, unstable_name_collision, unused_allocation,
+        unused_assignments, unused_attributes, unused_comparisons,
+        unused_doc_comment, unused_features, unused_imports, unused_macros,
+        unused_must_use, unused_mut, unused_parens, unused_unsafe,
+        unused_variables, while_true, exceeding_bitshifts,
+        invalid_type_param_default, legacy_constructor_visibility,
+        legacy_directory_ownership, legacy_imports,
+        missing_fragment_specifier, mutable_transmutes,
+        no_mangle_const_items, parenthesized_params_in_types_and_modules,
+        pub_use_of_private_extern_crate, safe_extern_statics,
+        unknown_crate_types)]
+#![warn(dead_code, unreachable_pub)]
+#![allow(box_pointers, elided_lifetime_in_path, unsafe_code, trivial_casts,
+         single_use_lifetime, trivial_numeric_casts)]
 // extern  ====================================================================
 extern crate env_logger;
 // ----------------------------------------------------------------------------
@@ -43,10 +52,10 @@ extern crate getopts;
 // use  =======================================================================
 use std::path::PathBuf;
 // ----------------------------------------------------------------------------
+use self::error::{Error, Result};
 use column79::{Column79, Command, Flags};
 // mod  =======================================================================
-#[macro_use]
-mod unwrap;
+mod error;
 // ////////////////////////////////////////////////////////////////////////////
 // ============================================================================
 fn print_usage(opts: &::getopts::Options, program: &str) {
@@ -68,7 +77,7 @@ Input:
     );
 }
 // ============================================================================
-fn main() {
+fn main() -> Result<()> {
     env_logger::init();
 
     let args: Vec<String> = ::std::env::args().collect();
@@ -76,7 +85,12 @@ fn main() {
     let _ = opts.optflag("v", "version", "print version")
         .optflag("h", "help", "print this help menu")
         .optopt("c", "column", "set column number", "NUM")
-        .optopt("t", "threshold", "set separator threshold number", "NUM")
+        .optopt(
+            "t",
+            "threshold",
+            "set separator threshold number",
+            "NUM",
+        )
         .optopt(
             "l",
             "language",
@@ -84,34 +98,56 @@ fn main() {
             "LANG",
         )
         .optflag("", "no-ask", "will not be asked to allow");
-    let matches = unwrap!(opts.parse(&args[1..]));
+    let matches = opts.parse(&args[1..])?;
     if matches.opt_present("v") {
-        println!(concat!(module_path!(), " v", env!("CARGO_PKG_VERSION")));
+        println!(concat!(
+            module_path!(),
+            " v",
+            env!("CARGO_PKG_VERSION")
+        ));
     }
     if matches.free.is_empty() || matches.opt_present("h") {
-        return print_usage(&opts, args[0].as_ref());
+        print_usage(&opts, args[0].as_ref());
+        return Ok(());
     }
     let command = Command::from(matches.free[0].as_ref());
     if Command::Unknown == command {
-        return print_usage(&opts, args[0].as_ref());
+        print_usage(&opts, args[0].as_ref());
+        return Ok(());
     }
     let input = if 1usize == matches.free.len() {
-        unwrap!(::std::env::current_dir())
+        ::std::env::current_dir()?
     } else {
         PathBuf::from(matches.free[1].clone())
     };
     let column = if matches.opt_present("c") {
-        Some(unwrap!(unwrap!(matches.opt_str("c")).parse::<usize>()))
+        Some(matches
+            .opt_str("c")
+            .ok_or_else(|| {
+                Error::OptionNone(
+                    "column79: matches.opt_str(\"c\").".to_string(),
+                )
+            })?
+            .parse::<usize>()?)
     } else {
         None
     };
     let septhr = if matches.opt_present("t") {
-        Some(unwrap!(unwrap!(matches.opt_str("t")).parse::<usize>()))
+        Some(matches
+            .opt_str("t")
+            .ok_or_else(|| {
+                Error::OptionNone(
+                    "column79: matches.opt_str(\"t\").".to_string(),
+                )
+            })?
+            .parse::<usize>()?)
     } else {
         None
     };
     let language = if matches.opt_present("l") {
-        Some(unwrap!(matches.opt_str("l")))
+        Some(matches.opt_str("l").ok_or_else(|| {
+            Error::OptionNone("column79: matches.opt_str(\"l\").".to_string())
+        })?)
     } else {
         None
     };
@@ -119,5 +155,6 @@ fn main() {
     if matches.opt_present("no-ask") {
         fs.insert(Flags::NOASK);
     }
-    unwrap!(Column79::run(command, input, language, column, septhr, fs));
+    Column79::run(command, input, language, column, septhr, fs)?;
+    Ok(())
 }
